c5a74062d58d646abea53d276cc9b775bb60b056
Aurora Moreira <auroramoreira@MacBook-Air-de-Aurora.local>
2025-05-08 17:00:56 -0300

    Commentary and Splitted Functions

--- src/main.rs
+++ src/main.rs
@@ -13,45 +13,68 @@ fn write_file_from_arg(file_path: &String, text: String) {
     write!(output, "{}", text).unwrap();
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let transcript = read_file_from_arg(&args[1]);
-
-    let rg_server_apps = Regex::new(r"(?m)Server\nAPP\n— ([\s\S]*?)\n{2}").unwrap();
-    let server_apps_vec = rg_server_apps
+fn transcript_to_csv(transcript: String) -> String {
+    // ## REGEX EXPLAINATION ##
+    // Captures every character between
+    // "Server
+    //  APP"
+    // until it finds a double line break.
+    let server_app_text = Regex::new(r"(?m)Server\nAPP\n— ([\s\S]*?)\n{2}")
+        .unwrap()
         .captures_iter(transcript.as_str())
-        .filter_map(|cap| cap.get(1).map(|m| m.as_str()))
-        .collect::<Vec<_>>();
-    let server_apps = server_apps_vec.join("\n");
+        .filter_map(|cap| cap.get(1).map(|m| m.as_str())) // Maps every capture to a &str
+        .collect::<Vec<_>>() // Collects it to a vector
+        .join("\n"); // Joins every item into a String, divided by line breaks
 
-    let rg_date_newlines = Regex::new(r"(AM|PM|])(\n)").unwrap();
-    let date_newlines = rg_date_newlines
-        .replace_all(server_apps.as_str(), |caps: &regex::Captures| {
+    // ## REGEX EXPLAINATION ##
+    // Captures linebreaks after AM/PM/] and replaces it with a space
+    let breaklines_to_space = Regex::new(r"(AM|PM|])(\n)")
+        .unwrap()
+        .replace_all(server_app_text.as_str(), |caps: &regex::Captures| {
             format!("{} ", &caps[1])
         })
         .to_string();
 
-    let rg_add_semicolon = Regex::new(
+    // ## REGEX EXPLAINATION ##
+    // Captures:
+    // 1. Any space or "](space)" after a 4-digit number, AM or PM -- 2025]
+    // 2. (space)»(space)
+    // 3. (linebreak)[(digits): -- \n[4:
+    // 4. "(space)has been unfined(space)"
+    // 5. (digit)(space)by(space) -- 0 by
+    // 6. (space)for(space) or (space)for:(space)
+    let divide_columns_with_semicolons = Regex::new(
         r"(/\d{4}|AM|PM)( +|] +)|( » )|(\n\[\d+:)|( has been unfined )|(\d) by |( for | for: )",
     )
-    .unwrap();
-    let add_semicolon = rg_add_semicolon
-        .replace_all(date_newlines.as_str(), |caps: &regex::Captures| {
-            if let Some(m) = caps.get(1).or_else(|| caps.get(6)) {
-                format!("{};", m.as_str())
-            } else if let Some(m) = caps.get(4) {
-                format!("\n;{}", &m.as_str()[2..])
-            } else {
-                ";".to_string()
-            }
-        })
-        .to_string();
+    .unwrap()
+    .replace_all(breaklines_to_space.as_str(), |caps: &regex::Captures| {
+        // WARNING: the number groups won't match
+        // if it's the 1st or 5th capture group
+        if let Some(m) = caps.get(1).or_else(|| caps.get(6)) {
+            format!("{};", m.as_str())
+        // if it's the 3rd capture group
+        } else if let Some(m) = caps.get(4) {
+            format!("\n;{}", &m.as_str()[2..])
+        // whatever else
+        } else {
+            ";".to_string()
+        }
+    })
+    .to_string();
 
-    let rg_remove_dates = Regex::new(r"\w+ \d*, \d+\n").unwrap();
-    let remove_dates = rg_remove_dates.replace_all(add_semicolon.as_str(), "");
+    // ## REGEX EXPLAINATION ##
+    // Captures a date with the format MMM dd, yyyy(linebreak)
+    let remove_dates = Regex::new(r"\w+ \d*, \d+\n")
+        .unwrap()
+        .replace_all(divide_columns_with_semicolons.as_str(), "");
 
-    println!("{}", &remove_dates);
+    remove_dates.to_string()
+}
 
-    write_file_from_arg(&args[2], remove_dates.to_string());
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let csv_file = transcript_to_csv(read_file_from_arg(&args[1].to_string()));
+
+    write_file_from_arg(&args[2], csv_file.to_string());
 }
